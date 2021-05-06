caf85ce43e1de9bc3088df44c6bdc0d1d4e3dfe8
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-05-06 19:39:25 -0300

    Cria Recipe

---
+++ app/Models/Recipe.ts
@@ -0,0 +1,29 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, BelongsTo, belongsTo } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+
+export default class Recipe extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public instructions: string
+
+  @column()
+  public image: string
+
+  @column()
+  public userId: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+}
